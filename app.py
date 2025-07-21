1eb891883e68f48d4424529091bbf2033d8a72e3
Gnanendra Reddy <gnanendrareddy.g@makeskilled.com>
2025-07-21 16:16:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'your-secret-key-here')
 
 # MongoDB connection
-client = MongoClient(os.getenv('MONGODB_URI', 'mongodb://localhost:27017/'))
+client = MongoClient(os.getenv('MONGODB_URI', 'mongodb+srv://arjun:arjun123@blog.dcjsvwf.mongodb.net/?retryWrites=true&w=majority&appName=Blog'))
 db = client['blogging_platform']
 users_collection = db['users']
 blogs_collection = db['blogs']
